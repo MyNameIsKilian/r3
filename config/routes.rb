37129a73ec2903b25a9c74c3a3fd35ef8a17c111
Kilian Alliot <kiliantravail@gmail.com>
2021-05-25 12:53:51 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :games, only: [:index, :show] do
+    resources :rounds, only: [:new, :create]
+  end
+
+  resources :researches, only: [:new, :create, :show]
 end
