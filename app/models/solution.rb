8012ae102f5f8ee2e4840c5087441be842dbb180
Valentina Banzet <valentina.banzet@gmail.com>
2021-06-01 14:46:05 +0200

    search mecanism

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -7,4 +7,12 @@ class Solution < ApplicationRecord
     }
   belongs_to :category
   validates :object_name, presence: true
+
+  def bien_jeter
+    content.split('Bien jeter : ')[1]
+  end
+
+  def first_content
+    content.split('Bien jeter : ').first
+  end
 end
