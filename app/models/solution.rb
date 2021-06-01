c5734a901f2e25b4daf1ddb5c31ec3a2bdad902d
Valentina Banzet <valentina.banzet@gmail.com>
2021-06-01 15:02:51 +0200

    fixing problem

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,4 +1,13 @@
 class Solution < ApplicationRecord
+
   belongs_to :category
   validates :object_name, presence: true
+
+  def bien_jeter
+    content.split('Bien jeter : ')[1]
+  end
+
+  def first_content
+    content.split('Bien jeter : ').first
+  end
 end
