e9a0b670790765d53483d96b34ae5d471bd63e42
Valentina Banzet <valentina.banzet@gmail.com>
2021-06-03 12:56:17 +0200

    front games and home

--- app/models/research.rb
+++ app/models/research.rb
@@ -2,6 +2,6 @@ class Research < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :solution
-  validates :query, presence: true, length: { in: 2..30 }
+  validates :query, presence: true, length: { in: 2..60 }
   validates :category, presence: true
 end
